a527d2cc492dfbe7ad9eb77747771bda98aacd6b
Hasham Malik <hasham2@gmail.com>
2012-06-12 12:56:16 +0500

    intial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+  
+  def index = Action {
+    Redirect(routes.Application.tasks)
+  }
+  
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
+
+}
